7e50da9b17cef6336e66829ec1918b3344a927d5
Harshali Chaudhari <harshalichaudhari19297@gmail.com>
2025-03-11 20:48:07 +0100

    Initial Commit

---
+++ CatchTheKenny/CatchTheKennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennyApp.swift
+//  CatchTheKenny
+//
+//  Created by Harshali Chaudhari on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheKennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchTheKenny/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CatchTheKenny
+//
+//  Created by Harshali Chaudhari on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("kenny")
+    }
+}
+
+#Preview {
+    ContentView()
+}
