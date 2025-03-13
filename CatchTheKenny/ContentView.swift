455d10835ab5b7528fec07d3fc5803b8b078393f
Harshali Chaudhari <harshalichaudhari19297@gmail.com>
2025-03-13 21:22:12 +0100

    Added Conter Timer. Tap gesture to get kenny moving on tap and alert on time over.

--- CatchTheKenny/ContentView.swift
+++ CatchTheKenny/ContentView.swift
@@ -13,21 +13,36 @@ struct ContentView: View {
     @State var timeLeft: Double = 10.0
     @State var chosenX: CGFloat = 200
     @State var chosenY: CGFloat = 200
+    @State var showAlert: Bool = false
     
     //Position Tuples
     let x1 = (60,60)
-    let x2 = (120,120)
+    let x2 = (70,240)
     let x3 = (180,180)
     let x4 = (180,300)
-    let x5 = (300,300)
-    let x6 = (340,340)
+    let x5 = (300,120)
+    let x6 = (340,80)
     let x7 = (120,180)
-    let x8 = (240,240)
+    let x8 = (170,240)
     let x9 = (240,120)
     let x10 = (300,240)
-    let x11 = (340,300)
-    let x12 = (340,60)
+    let x11 = (340,130)
+    let x12 = (180,180)
     
+    //Timer for Time Left
+    var counterTimer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if self.timeLeft < 0.5 {
+                self.chosenX = 200
+                self.chosenY = 200
+                self.showAlert = true
+            } else {
+                self.timeLeft -= 1
+            }
+        }
+    }
+    
+    //Timer for changing Kenny's positions
     var timer: Timer {
         
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
@@ -73,11 +88,25 @@ struct ContentView: View {
                 .resizable()
                 .frame(width: 180,height: 100)
                 .position(x: self.chosenX, y: self.chosenY)
+                .onTapGesture(count: 1, perform: {
+                    //Increase score when tapped on Kenny
+                    self.score += 1
+                })
                 .onAppear {
+                    //Initiate Timers
                     _ = self.timer
+                    _ = self.counterTimer
                 }
             
             Spacer()
+        }.alert(isPresented: $showAlert) {
+            //Show Alert when time is over
+            return Alert(title: Text("Oops..Time Over!!"), message: Text("Your score is: \(self.score)\n Want to Play Again?"),
+                primaryButton: Alert.Button.default(Text("Ok"), action: {
+                //OK Button Action
+                self.score = 0
+                self.timeLeft = 10.0
+            }), secondaryButton: Alert.Button.cancel())
         }
         
     }
