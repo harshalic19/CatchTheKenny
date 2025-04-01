51b66dab7529e05ff6bb0da9a8f0847c00f9c592
Harshali Chaudhari <harshalichaudhari19297@gmail.com>
2025-04-01 20:53:33 +0200

    Solved Alert's cancel button issue and some improvements.

--- CatchTheKenny/ContentView.swift
+++ CatchTheKenny/ContentView.swift
@@ -14,54 +14,32 @@ struct ContentView: View {
     @State var chosenX: CGFloat = 200
     @State var chosenY: CGFloat = 200
     @State var showAlert: Bool = false
+    @State var gameTimer: Timer?
+    @State var counterTimer: Timer?
     
-    //Position Tuples
-    let x1 = (60,60)
-    let x2 = (70,240)
-    let x3 = (180,180)
-    let x4 = (180,300)
-    let x5 = (300,120)
-    let x6 = (340,80)
-    let x7 = (120,180)
-    let x8 = (170,240)
-    let x9 = (240,120)
-    let x10 = (300,240)
-    let x11 = (340,130)
-    let x12 = (180,180)
+    // Position Tuples
+    let positions = [(60,60), (70,240), (180,180), (180,300), (300,120), (340,80), (120,180), (170,240), (240,120), (300,240), (340,130), (180,180)]
     
-    //Timer for Time Left
-    var counterTimer: Timer {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            if self.timeLeft < 0.5 {
-                self.chosenX = 200
-                self.chosenY = 200
+    func startTimers() {
+        gameTimer?.invalidate()
+        counterTimer?.invalidate()
+        
+        // Timer for moving Kenny
+        gameTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            let newPosition = positions.randomElement()!
+            self.chosenX = CGFloat(newPosition.0)
+            self.chosenY = CGFloat(newPosition.1)
+        }
+        
+        // Timer for countdown
+        counterTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            if self.timeLeft <= 1 {
+                timer.invalidate()
+                gameTimer?.invalidate()
                 self.showAlert = true
             } else {
                 self.timeLeft -= 1
             }
-        }
-    }
-    
-    //Timer for changing Kenny's positions
-    var timer: Timer {
-        
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            
-            let positionArr = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12]
-            
-            var previousnum: Int?
-            
-            func randomNumber() -> Int {
-                var randomNumber: Int
-                repeat {
-                    randomNumber = Int.random(in: 0..<positionArr.count)
-                } while previousnum == randomNumber
-                previousnum = randomNumber
-                return randomNumber
-            }
-            
-            self.chosenX = CGFloat(positionArr[randomNumber()].0)
-            self.chosenY = CGFloat(positionArr[randomNumber()].1)
         }
     }
     
@@ -94,19 +72,23 @@ struct ContentView: View {
                 })
                 .onAppear {
                     //Initiate Timers
-                    _ = self.timer
-                    _ = self.counterTimer
+                    startTimers()
                 }
             
             Spacer()
         }.alert(isPresented: $showAlert) {
             //Show Alert when time is over
             return Alert(title: Text("Oops..Time Over!!"), message: Text("Your score is: \(self.score)\n Want to Play Again?"),
-                primaryButton: Alert.Button.default(Text("Ok"), action: {
+                         primaryButton: Alert.Button.default(Text("Ok"), action: {
                 //OK Button Action
                 self.score = 0
                 self.timeLeft = 10.0
-            }), secondaryButton: Alert.Button.cancel())
+                self.showAlert = false
+                startTimers()
+            }),  secondaryButton: .cancel(Text("Cancel"), action: {
+                gameTimer?.invalidate()
+                counterTimer?.invalidate()
+            }))
         }
         
     }
